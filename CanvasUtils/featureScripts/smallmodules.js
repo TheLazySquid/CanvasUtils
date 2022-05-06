8aecb5abe5b74d7f2100d73a595b293010878f64
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2022-05-06 12:47:07 -0400

    Delete CanvasUtils directory

--- CanvasUtils/featureScripts/smallmodules.js
+++
@@ -1,9 +0,0 @@
-chrome.storage.sync.get(['closeModules'], (value) => {
-    if(!value.closeModules) return;
-    if(location.href.includes("/modules")){
-        let modules = document.querySelectorAll(".content");
-        for(let module of modules){
-            module.style.display = "none";
-        }
-    }
-})
